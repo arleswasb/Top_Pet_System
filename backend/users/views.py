8c9218472c947e40cd26d5dc843f7947f45844ff
arleswasb <werbert.barradas.390@ufrn.edu.br>
2025-06-22 18:30:58 -0300

    segunda atualização de users.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 from rest_framework import generics, status, permissions
 from django.conf import settings
 from .permissions import IsAdminRole # Importa nossa nova permissão
