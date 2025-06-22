80b354e9649858fab3d27fe9786abd11e73827f5
arleswasb <werbert.barradas.390@ufrn.edu.br>
2025-06-22 20:05:51 -0300

    correções gerais

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,19 +3,8 @@
 # Garante que o script irá parar se algum comando falhar
 set -e
 
-# Espera o banco de dados ficar pronto (opcional mas recomendado)
-# (Isso pode exigir a instalação do netcat no seu Dockerfile: RUN apt-get update && apt-get install -y netcat)
-# while ! nc -z db 5432; do
-#   echo "Aguardando o PostgreSQL iniciar..."
-#   sleep 1
-# done
-# echo "PostgreSQL iniciado."
-
-# Aplicando migrações do banco de dados
 echo "Aplicando migrações do banco de dados..."
 python manage.py migrate --no-input
 
-# A linha mágica: executa o comando principal que foi passado para o contêiner.
-# Se você rodar 'docker compose run web python manage.py test', "$@" será 'python manage.py test'.
-# Se você rodar 'docker compose up', "$@" será o comando padrão do seu Dockerfile (provavelmente 'runserver').
+# A linha mágica: executa o comando principal (CMD) que foi passado para o contêiner.
 exec "$@"
