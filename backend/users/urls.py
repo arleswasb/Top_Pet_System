e1007dc095ae91fce8c7fc61a6932fded2d073eb
arleswasb <werbert.barradas.390@ufrn.edu.br>
2025-06-22 18:38:12 -0300

    atualização da urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,11 @@
+# users/urls.py
+
 from django.urls import path
-from .views import LogFileView
+# Importe TODAS as views que você usa neste arquivo
+from .views import LogFileView, UserCreateView
 
 urlpatterns = [
     path('logs/', LogFileView.as_view(), name='get-logs'),
     path('register/', UserCreateView.as_view(), name='user-register'),
+    
 ]
